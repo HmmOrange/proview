7aece9de14a951d20b3bab1ee030b14a0e155923
HmmOrange <leanhduy0206@gmail.com>
2024-10-10 16:12:03 +0700

    Remove unnecessary bits in code

--- src/main/java/org/example/demo/AppMain.java
+++ src/main/java/org/example/demo/AppMain.java
@@ -34,8 +34,6 @@ public class AppMain extends Application {
     }
 
     public static void main(String[] args) {
-
-
         // Loading driver
         try {
             System.out.println(":3");
@@ -52,43 +50,6 @@ public class AppMain extends Application {
                     dotenv.get("SQL_USER"),
                     dotenv.get("SQL_PASSWORD")
             );
-
-//            Statement statement = connection.createStatement();
-//            statement.executeUpdate("""
-//                    CREATE TABLE IF NOT EXISTS document (
-//                        id INT PRIMARY KEY,
-//                        name VARCHAR(100),
-//                        author VARCHAR(100)
-//                    );
-//            """);
-//            statement.executeUpdate("""
-//                    DROP TABLE IF EXISTS document;
-//            """);
-//            Scanner sc = new Scanner(System.in);
-//            System.out.print("Number of queries: ");
-//            int queries = sc.nextInt();
-//            String sql = "INSERT INTO document (id, name, author) VALUES (?, ?, ?)";
-//            PreparedStatement st = connection.prepareStatement(sql);
-//            for (int q = 0; q < queries; q++) {
-//                int id = sc.nextInt();
-//                String name = sc.next();
-//                String author = sc.next();
-//
-//                st.setInt(1, id);       // Đặt id vào placeholder đầu tiên (?)
-//                st.setString(2, name);  // Đặt name vào placeholder thứ hai (?)
-//                st.setString(3, author); // Đặt author vào placeholder thứ ba (?)
-//            }
-//            /*statement.executeUpdate("""
-//                    INSERT INTO document (id, name, author) VALUES (id, name, author);
-//            """);*/
-//            st.executeUpdate();
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM document");
-//            while (resultSet.next()) {
-//                System.out.println(resultSet.getString("id"));
-//                System.out.println(resultSet.getString("name"));
-//                System.out.println(resultSet.getString("author"));
-//
-//            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
