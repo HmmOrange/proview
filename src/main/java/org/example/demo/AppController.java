81c43ffb36dcb6f816b872c2bf4e1d4049ec23d1
KwangZung <yungngq@gmail.com>
2024-10-10 14:40:06 +0700

    add database

--- src/main/java/org/example/demo/AppController.java
+++ src/main/java/org/example/demo/AppController.java
@@ -1,11 +1,14 @@
 package org.example.demo;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
 
 public class AppController {
     public TextField documentAddID;
@@ -15,28 +18,45 @@ public class AppController {
     public ListView<String> documentList;
     public Button addButton;
 
-    private void refreshDocumentListView() {
-        documentList.getItems().clear();
+    @FXML
+    public void initialize() {
+        reloadDocumentListView();
+    }
 
-        ObservableList<Document> currentDocumentList = DocumentAPI.getDocumentList();
+    private void reloadDocumentListView() {
+        System.out.println("reload");
+        try {
+            assert documentList != null;
+            documentList.getItems().clear();
+        } catch (NullPointerException ignored) {
+
+        }
+        ObservableList<Document> currentDocumentList = null;
+        try {
+            currentDocumentList = DB.getDocumentList();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         for (Document d : currentDocumentList) {
             String newDocumentItem = d.getId() + ". " + d.getName() + " - " + d.getAuthor();
             documentList.getItems().add(newDocumentItem);
         }
     }
-    @FXML
-    public void onAddButtonClick(ActionEvent actionEvent) {
-        Document newDocument = DocumentAPI.addDocument(
+
+    public void onAddButtonClick(ActionEvent actionEvent) throws SQLException {
+        Document newDocument = new Document(
                 Integer.parseInt(documentAddID.getText()),
                 documentAddName.getText(),
                 documentAddAuthor.getText()
         );
+
+        DB.addDocument (newDocument.getId(), newDocument.getName(), newDocument.getAuthor());
         String newDocumentItem = documentAddID.getText() + ". " + documentAddName.getText() + " - " + documentAddAuthor.getText();
         documentList.getItems().add(newDocumentItem);
     }
 
-    public void onRemoveButtonClick(ActionEvent actionEvent) {
-        DocumentAPI.removeDocument(Integer.parseInt(documentRemoveID.getText()));
-        refreshDocumentListView();
+    public void onRemoveButtonClick(ActionEvent actionEvent) throws SQLException {
+        DB.removeDocument(Integer.parseInt(documentRemoveID.getText()));
+        reloadDocumentListView();
     }
 }
