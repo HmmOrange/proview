a6d2f90a2b3c33153b2f31399203c69c32802663
KwangZung <yungnq@gmail.com>
2024-11-09 14:57:27 +0700

    add new User constructor with new arguments

--- src/main/java/org/proview/test/ProfileView.java
+++ src/main/java/org/proview/test/ProfileView.java
@@ -2,6 +2,7 @@ package org.proview.test;
 
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import org.proview.model.UserManagement;
 
 public class ProfileView {
     public Label usernameField;
@@ -9,6 +10,7 @@ public class ProfileView {
     public ImageView avatarImageView;
 
     public void initialize() {
-
+        usernameField.setText(UserManagement.getCurrentUser().getUsername());
+        emailField.setText(UserManagement.getCurrentUser().getEmail());
     }
 }
