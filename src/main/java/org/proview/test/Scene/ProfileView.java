6a8043df661e22c7242ca35ecbc626dd4b7d9e25
KwangZung <yungnq@gmail.com>
2024-11-15 10:23:11 +0700

    add editProfile feature with cannot_alter_username bug

--- src/main/java/org/proview/test/Scene/ProfileView.java
+++ src/main/java/org/proview/test/Scene/ProfileView.java
@@ -1,9 +1,16 @@
 package org.proview.test.Scene;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.proview.modal.User.Admin;
+import org.proview.modal.User.NormalUser;
 import org.proview.modal.User.UserManagement;
+import org.proview.test.AppMain;
 
 import java.io.*;
 import java.sql.SQLException;
@@ -12,6 +19,7 @@ public class ProfileView {
     public Label usernameField;
     public Label emailField;
     public ImageView avatarImageView;
+    public Button editProfileButton;
 
     public void initialize() throws SQLException, IOException {
         usernameField.setText(UserManagement.getCurrentUser().getUsername());
@@ -24,5 +32,20 @@ public class ProfileView {
         avatarImageView.setPreserveRatio(true);
         avatarImageView.setSmooth(true);
         avatarImageView.setCache(true);
+
+        if (UserManagement.getCurrentUser() instanceof Admin) {
+            editProfileButton.setDisable(true);
+            editProfileButton.setVisible(false);
+        }
+    }
+
+    public void onEditProfileButtonClick(ActionEvent actionEvent) throws IOException {
+        if (UserManagement.getCurrentUser() instanceof NormalUser) {
+            FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("EditProfileView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1300, 700);
+            AppMain.window.setTitle("Hello!");
+            AppMain.window.setScene(scene);
+            AppMain.window.centerOnScreen();
+        }
     }
 }
