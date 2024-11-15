1c68185b3ad15b9da5610b017fead3eb2be6fff3
KwangZung <yungnq@gmail.com>
2024-11-15 09:03:53 +0700

    alter register feature

--- src/main/java/org/proview/modal/User/UserManagement.java
+++ src/main/java/org/proview/modal/User/UserManagement.java
@@ -18,7 +18,7 @@ public class UserManagement {
 
     public static void addNormalUser(User user) throws SQLException {
         Connection connection = AppMain.connection;
-        Statement statement = connection.createStatement();
+        /*Statement statement = connection.createStatement();
         statement.executeUpdate(
         """
                 CREATE TABLE IF NOT EXISTS user (
@@ -33,12 +33,14 @@ public class UserManagement {
                 INSERT IGNORE INTO user (username, password, type)
                 VALUES ('admin', 'admin', 0);
             """
-        );
-        String sql = "INSERT IGNORE INTO user (username, password, type) VALUES (?, ?, 1)";
+        );*/
+        String sql = "INSERT IGNORE INTO user (username, password, type, firstname, lastname, email) VALUES (?, ?, 1, ?, ?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, user.getUsername());
         preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getFirstName());
+        preparedStatement.setString(4, user.getLastName());
+        preparedStatement.setString(5, user.getEmail());
         preparedStatement.executeUpdate();
-        System.out.println("123");
     }
 }
--- src/main/java/org/proview/test/Scene/RegisterView.java
+++ src/main/java/org/proview/test/Scene/RegisterView.java
@@ -1,9 +1,12 @@
 package org.proview.test.Scene;
 
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+import org.proview.modal.Book.BookManagement;
 import org.proview.modal.User.NormalUser;
 import org.proview.modal.User.User;
 import javafx.event.ActionEvent;
@@ -14,7 +17,10 @@ import javafx.scene.control.TextField;
 import org.proview.modal.User.UserManagement;
 import org.proview.test.AppMain;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,15 +39,37 @@ public class RegisterView {
     public TextField lastNameField;
     public TextField emailField;
 
+    private File avatarFile;
+
+    public void initialize() {
+        avatarFile = new File("./assets/samples/defaultAvatar.png");
+        if (avatarFile != null) {
+            Image image = new Image(avatarFile.toURI().toString());
+            if (image.isError()) {
+                System.out.println("Error loading image: " + image.getException().getMessage());
+            }
+            avatarField.setImage(image);
+            avatarField.setFitWidth(300);
+            avatarField.setPreserveRatio(true);
+            avatarField.setSmooth(true);
+            avatarField.setCache(true);
+        }
+    }
+
     public void onConfirmButtonClick(ActionEvent actionEvent) throws IOException, SQLException {
         if (Objects.equals(registerUsernameField.getText(), "") ||
-        Objects.equals(registerPasswordField.getText(), "") || Objects.equals(registerConfirmPasswordField.getText(), "")) {
+            Objects.equals(registerPasswordField.getText(), "") || Objects.equals(registerConfirmPasswordField.getText(), "") ||
+            Objects.equals(firstNameField.getText(), "") || Objects.equals(lastNameField.getText(), "")
+                || Objects.equals(emailField.getText(), "")) {
             registerResultLabel.setText("Please fill in all blanks");
         }
         else {
             String usn = registerUsernameField.getText();
             String pass = registerPasswordField.getText();
             String cfpass = registerConfirmPasswordField.getText();
+            String firstName = firstNameField.getText();
+            String lastName = lastNameField.getText();
+            String email = emailField.getText();
 
             /// check if username exists
             String sql = "SELECT username FROM user WHERE username = ?";
@@ -54,8 +82,23 @@ public class RegisterView {
             if (!Objects.equals(pass, cfpass)) registerResultLabel.setText("Password wrong");
             else if (resultSet.next()) registerResultLabel.setText("Username already exists!");
             else {
-                User us = new NormalUser(usn, pass);
-                UserManagement.addNormalUser(us);
+                ///get the latest id
+                int latest_id = 0;
+                String idSql = "SELECT MAX(id) AS latest_id FROM user";
+                PreparedStatement idPS = connection.prepareStatement(idSql);
+                ResultSet idRS = idPS.executeQuery();
+                if(idRS.next()) {
+                    latest_id = idRS.getInt("latest_id");
+                } else System.out.println("Cannot find latest ID");
+                ///
+
+                ///add avatar to folder
+                String dstFilePath = "./assets/avatars/user" + (latest_id + 1) + ".png";
+                Files.copy(avatarFile.toPath(), (new File(dstFilePath)).toPath(), StandardCopyOption.REPLACE_EXISTING);
+                ///
+
+                User user = new NormalUser(latest_id + 1, usn, pass, firstName, lastName, email);
+                UserManagement.addNormalUser(user);
                 registerResultLabel.setText("Register Success");
 
                 FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("LoginView.fxml"));
@@ -79,43 +122,27 @@ public class RegisterView {
 
     public void onKeyReleased(KeyEvent keyEvent) throws SQLException, IOException {
         if (keyEvent.getCode() == KeyCode.ENTER) {
-            if (Objects.equals(registerUsernameField.getText(), "") ||
-                    Objects.equals(registerPasswordField.getText(), "") || Objects.equals(registerConfirmPasswordField.getText(), "")) {
-                registerResultLabel.setText("Please fill in all blanks");
-            }
-            else {
-                String usn = registerUsernameField.getText();
-                String pass = registerPasswordField.getText();
-                String cfpass = registerConfirmPasswordField.getText();
-
-                /// check if username exists
-                String sql = "SELECT username FROM user WHERE username = ?";
-                Connection connection = AppMain.connection;
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setString(1, usn);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                ///
-
-                if (!Objects.equals(pass, cfpass)) registerResultLabel.setText("Password wrong");
-                else if (resultSet.next()) registerResultLabel.setText("Username already exists!");
-                else {
-                    User user = new NormalUser(usn, pass);
-                    UserManagement.addNormalUser(user);
-                    registerResultLabel.setText("Register Success");
-
-                    FXMLLoader fxmlLoader = new FXMLLoader(AppMain.class.getResource("LoginView.fxml"));
-                    Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-                    LoginView tempLoginView = fxmlLoader.getController();
-                    tempLoginView.loginResultLabel.setText("Register Success. Please log in!");
-                    AppMain.window.setTitle("Hello!");
-                    AppMain.window.setScene(scene);
-                    AppMain.window.centerOnScreen();
-                }
-            }
+            onConfirmButtonClick(new ActionEvent());
         }
     }
 
     public void onAddAvatarButtonClick(ActionEvent actionEvent) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose book cover");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.gif")
+        );
+        avatarFile = fileChooser.showOpenDialog(AppMain.window);
+        if (avatarFile != null) {
+            Image image = new Image(avatarFile.toURI().toString());
+            if (image.isError()) {
+                System.out.println("Error loading image: " + image.getException().getMessage());
+            }
+            avatarField.setImage(image);
+            avatarField.setFitWidth(300);
+            avatarField.setPreserveRatio(true);
+            avatarField.setSmooth(true);
+            avatarField.setCache(true);
+        }
     }
 }
