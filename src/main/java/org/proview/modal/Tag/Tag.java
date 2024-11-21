a08659f67922020893f3868e8116ce222f2ae9ab
HmmOrange <leanhduy0206@gmail.com>
2024-11-22 00:23:04 +0700

    Unfinished tag management

--- src/main/java/org/proview/modal/Tag/Tag.java
+++ src/main/java/org/proview/modal/Tag/Tag.java
@@ -1,6 +1,14 @@
 package org.proview.modal.Tag;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import org.proview.test.AppMain;
+import org.proview.test.Container.TagView;
+
+import java.io.IOException;
+
 public class Tag extends TagStyle {
+    private Label label;
     private String tagName;
 
     public Tag(String tagName, String bgColorHex, String textColorHex) {
@@ -13,6 +21,28 @@ public class Tag extends TagStyle {
         return tagName;
     }
 
+    public Label getLabel() {
+        if (label == null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(AppMain.class.getResource("TagView.fxml"));
+                label = loader.load();
+                TagView tagView = loader.getController();
+                tagView.setData(
+                        tagName,
+                        super.getBgColorHex(),
+                        super.getTextColorHex()
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return label;
+    }
+
+    public void setLabel(String tagName, String bgColorHex, String textColorHex) {
+
+    }
     @Override
     public String toString() {
         return tagName;
--- src/main/java/org/proview/test/Scene/TagManageView.java
+++ src/main/java/org/proview/test/Scene/TagManageView.java
@@ -1,29 +1,91 @@
 package org.proview.test.Scene;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
+import org.proview.modal.Tag.Tag;
+import org.proview.utils.SQLUtils;
 
+import java.sql.SQLException;
 import java.util.Comparator;
 
 public class TagManageView {
-    public TableView<ObservableList<String>> tagTable;
+    public static class RowData {
+        private final Tag tag;
+
+        public RowData(Tag tag) {
+            this.tag = tag;
+        }
+
+        public Tag getTag() {
+            return tag;
+        }
+
+        public String getCol(int idx) {
+            return switch (idx) {
+                case 0 -> tag.getTagName();
+                case 1 -> tag.getBgColorHex();
+                case 2 -> tag.getTextColorHex();
+                default -> "";
+            };
+
+        }
+    }
+
+    public ObservableList<RowData> rowDataList = FXCollections.observableArrayList();
+    public TableView<RowData> tagTable;
     public TextField tagEditNameLabel;
     public TextField tagEditBgColorHex;
     public TextField tagEditTextColorHex;
 
-    public void initialize() {
+    public void initTable() {
         String[] columnName = {"Tag Name", "Tag Background Color", "Tag Text Color", "Tag Preview"};
-        for (int i = 0; i < 3; i++) {
-            TableColumn<ObservableList<String>, String> column = new TableColumn<>(columnName[i]);
+
+        // First three columns
+        for (int i = 0; i < columnName.length - 1; i++) {
+            TableColumn<RowData, String> column = new TableColumn<>(columnName[i]);
             int finalI = i;
-            column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(finalI)));
+            column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCol(finalI)));
+            column.setComparator(Comparator.comparing(String::toString));
             tagTable.getColumns().add(column);
         }
 
         // For the last column, use Tag & TagView instead of String
-        TableColumn<ObservableList<String>, String> column = new TableColumn<>(columnName[columnName.length - 1]);
-        column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().get(columnName.length - 1)));
-        column.setCellFactory(column -> );
+        TableColumn<RowData, Tag> tagColumn = new TableColumn<>("Tag");
+        tagColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getTag()));
+        tagColumn.setCellFactory(col -> new TableCell<>() {
+            @Override
+            protected void updateItem(Tag tag, boolean empty) {
+                super.updateItem(tag, empty);
+                if (empty || tag == null) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(tag. getLabel());
+                }
+            }
+        });
+        tagTable.getColumns().add(tagColumn);
+
+    }
+
+    public void loadNodes() throws SQLException {
+
+    }
+    public void reloadTable() throws SQLException {
+        rowDataList.clear();
+        ObservableList<Tag> tagList = SQLUtils.getTagList();
+        for (Tag tag: tagList) {
+            rowDataList.add(new RowData(tag));
+        }
+
+        tagTable.setItems(rowDataList);
+    }
+    public void initialize() throws SQLException {
+        initTable();
+        reloadTable();
+        loadNodes();
     }
 }
