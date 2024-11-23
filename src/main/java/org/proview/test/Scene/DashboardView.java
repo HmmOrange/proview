c11f1471d9b5d517d00907fb8cb7398808282cb8
KwangZung <yungnq@gmail.com>
2024-11-23 23:46:35 +0700

    add issueCount per day line chart in dashboard

--- src/main/java/org/proview/test/Scene/DashboardView.java
+++ src/main/java/org/proview/test/Scene/DashboardView.java
@@ -2,13 +2,15 @@ package org.proview.test.Scene;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.VBox;
 import org.proview.utils.DashboardUtils;
 
 import java.sql.SQLException;
 
 public class DashboardView {
+
     private void initCopiesBorrowedPieChart() throws SQLException {
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
                 new PieChart.Data("Borrowed", DashboardUtils.CopiesBorrowedPieChart.getCopiesBorrowed()),
@@ -24,10 +26,30 @@ public class DashboardView {
         }
     }
 
+    private void initIssueCreatedNumberLineChart() throws SQLException {
+        XYChart.Series<String, Integer> datas = DashboardUtils.IssuesNumberLineChart.getChartData();
+        issuesCreatedNumberLineChart.getData().add(datas);
+        for (XYChart.Data<String, Integer> data : datas.getData()) {
+            String tooltipString = data.getXValue() + ": ";
+            int yValue = data.getYValue();
+            if (yValue == 0) {
+                tooltipString += "No new issue.";
+            } else if (yValue == 1) {
+                tooltipString += "1 new issue.";
+            } else {
+                tooltipString += yValue + " new issues.";
+            }
+            Tooltip tooltip = new Tooltip(tooltipString);
+            Tooltip.install(data.getNode(), tooltip);
+        }
+    }
+
     public PieChart copiesBorrowedPieChart;
+    public LineChart<String, Integer> issuesCreatedNumberLineChart;
 
     public void initialize() throws SQLException {
         initCopiesBorrowedPieChart();
+        initIssueCreatedNumberLineChart();
     }
 
 }
